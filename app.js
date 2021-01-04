894be2d138b70ae98b9ba8f0de73aa6dd1224837
jswj30 <jswj30@naver.com>
2021-01-04 17:27:38 +0900

    modify app.js

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
-const session = require('express-session');
-const cors = require('cors');
+const session = require("express-session");
+const cors = require("cors");
 
 app.use(
-    cors({
-        credentials: true
-    }));
+  cors({
+    origin: ["https://get-todo.com"],
+    methods: ["GET", "POST", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(
-    session({
-        secret: 'secretkey',
-        resave: false,
-        saveUninitialized: true
-    })
+  session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // router
-const indexRouter = require('./routes/index');
-const searchRouter = require('./routes/search');
+const indexRouter = require("./routes/index");
+const searchRouter = require("./routes/search");
 
-app.use('/', indexRouter);
-app.use('/searchinfo', searchRouter);
+app.use("/", indexRouter);
+app.use("/searchinfo", searchRouter);
 
 // get, post
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
 
 // 서버 실행
 app.listen(port, () => {
-    console.log(`fin.k.l(c) listening at http://localhost:${port}`);
+  console.log(`fin.k.l(c) listening at http://localhost:${port}`);
 });
