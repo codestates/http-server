e54f383a445b8bde3db90ebab9236b07208c9d32
jswj30 <jswj30@naver.com>
2021-01-06 11:51:55 +0900

    modify session_10

--- controller/user/main.js
+++ controller/user/main.js
@@ -1,5 +1,5 @@
-const e = require("express");
-const s = require("express-session");
+const express = require("express");
+const session = require("express-session");
 const { Todo, User, Complete, JoinTable } = require("../../models");
 // const session = require('express-session');
 // const jwt = require('jsonwebtoken');
